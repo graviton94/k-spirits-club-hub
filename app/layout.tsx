d9738433b74edff88feab20b1a585cf37c9a1800
graviton <ruahn49@gmail.com>
2026-02-01 15:35:23 +0900

    태그 위치수정..

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,28 +69,29 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         {/* Google Tag Manager - GTM-NDF5RKBN */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NDF5RKBN');`}
-        </Script>
+            })(window,document,'script','dataLayer','GTM-NDF5RKBN');`,
+          }}
+        />
 
         {/* Google tag (gtag.js) - G-0QF9WTQFF2 */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-0QF9WTQFF2"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0QF9WTQFF2" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-            gtag('config', 'G-0QF9WTQFF2');
-          `}
-        </Script>
+              gtag('config', 'G-0QF9WTQFF2');
+            `,
+          }}
+        />
 
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
